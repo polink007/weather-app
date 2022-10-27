9c4bb7792465e5c85d42c69231e9d8c15891d3a6
polink007 <116698367+polink007@users.noreply.github.com>
2022-10-26 22:16:31 -0400

    Added existing project

---
+++ src/index.js
@@ -0,0 +1,103 @@
+///// This is current date and time /////
+let now = new Date();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let day = days[now.getDay()];
+let time = now.toLocaleTimeString([], { hour: "2-digit", minute: "2-digit" });
+
+let today = document.querySelector("#today-day");
+today.innerHTML = `${day} ${time}`;
+
+///// This is API call for weather by city /////
+function callApiForCityOrCoordinates(searchParameters = "q=new york") {
+  let apiKey = "197ef3a642b76eef90e131866f74a0a0";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?${searchParameters}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayWeather);
+}
+
+// Here we get data from response and insert into corresponding elements
+function displayWeather(response) {
+  // create variables and asign corresponding HTML elements
+  let placeElement = document.querySelector("#current-location");
+  let tempElement = document.querySelector("#current-temp");
+  let feelsLikeElement = document.querySelector("#real-feel");
+  let humidityElement = document.querySelector("#humidity");
+  let windElement = document.querySelector("#wind");
+  let sunriseElement = document.querySelector("#sunrise");
+  let sunsetElement = document.querySelector("#sunset");
+
+  // create variables and asign corresponding data from API response
+  let data = response.data;
+  let city = data.name;
+  let country = data.sys.country;
+  let temp = `${Math.round(data.main.temp)}°`;
+  let tempFeels = `${Math.round(data.main.feels_like)}°`;
+  let humidity = data.main.humidity;
+  let wind = `${Math.round(data.wind.speed)}`;
+  let sunriseLocalTime = convertUnixtoLocalTime(data.sys.sunrise);
+  let sunsetLocalTime = convertUnixtoLocalTime(data.sys.sunset);
+
+  // insert collected data into corresponding HTMl elements
+  placeElement.innerHTML = `${city}, ${country}`;
+  tempElement.innerHTML = temp;
+  feelsLikeElement.innerHTML = tempFeels;
+  humidityElement.innerHTML = `${humidity}%`;
+  windElement.innerHTML = `${wind} meter/sec`;
+  sunriseElement.innerHTML = sunriseLocalTime;
+  sunsetElement.innerHTML = sunsetLocalTime;
+}
+
+function convertUnixtoLocalTime(timeStamp) {
+  let DateObject = new Date(timeStamp * 1000);
+  let localTime = DateObject.toLocaleTimeString([], {
+    hour: "2-digit",
+    minute: "2-digit",
+  });
+
+  return localTime;
+}
+
+///// Search weather by city /////
+
+// Event listeners for search button and enter
+let searchInput = document.querySelector("#search-input");
+let searchCityButton = document.querySelector("#button-addon2");
+
+searchCityButton.addEventListener("click", searchWeatherForCity);
+searchInput.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") {
+    searchWeatherForCity();
+  }
+});
+
+function searchWeatherForCity() {
+  let searchValue = searchInput.value;
+  let searchParameters = `q=${searchValue}`;
+
+  callApiForCityOrCoordinates(searchParameters);
+}
+
+///// Search weather for current location /////
+let button = document.querySelector("#current-location-button");
+button.addEventListener("click", function () {
+  navigator.geolocation.getCurrentPosition(getWeatherForCoordinates);
+});
+
+function getWeatherForCoordinates(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let searchParameters = `lat=${lat}&lon=${lon}`;
+  callApiForCityOrCoordinates(searchParameters);
+}
+
+callApiForCityOrCoordinates();
